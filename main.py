dbf85913f20eeef7462e7a25bbaac5f6f5f7f970
Vanessa Landayan <>
2021-05-01 23:56:06 +0000

    updating commands

--- main.py
+++ main.py
@@ -3,71 +3,77 @@ import os
 from discord.ext import commands
 from stay_awake import stay_awake
 
+# intents = discord.Intents().all()
+# client = discord.Bot(prefix='', intents=intents)
 bot = commands.Bot(command_prefix = '$')
 
 stay_awake()
 # bot = discord.Client()
 
-bot.add_cog(Points(bot))
-bot.add_cog(HallPass(bot))
+# Bot Setup
+@bot.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(bot))
 
-# Commands
-
+# Messages
 async def on_message(message):
   # Ignore messages from bot itself
   if message.author == bot:
     return
   
-  # Create a new channel (i.e. assignments, discussion groups,...)
-  if message.content.startswith('$create'):
-    channel_name = message.content[7:].strip()
-    await message.channel.category.create_text_channel(channel_name)
+# Create a new channel (i.e. assignments, discussion groups,...)
+@bot.command()
+async def create(ctx, channelName):
+  guild = ctx.guild
+  await guild.create_text_channel(name='{}'.format(channelName), category=int(837874749211541506))
 
+@bot.command()
+async def new(ctx, category, name):
+  guild = ctx.message.guild
+  if category == "assignment":
+    await guild.create_text_channel(name)
+  elif category == "q":
 
 # Repeat message in the Question channel
 @bot.command()
 async def question(ctx, *, arg):
-  channel = client.get_channel(838130738729189448)
+  channel = bot.get_channel(838130738729189448)
   await channel.send(arg)
 
 
 # Points
-class Points(commands.Cog):
-  def __init__(self, bot):
-      self.bot = bot
-  
-  @commands.command()
-  async def add(self, ctx, student: discord.Member):
+@bot.event
+async def add(self, ctx, student: discord.Member):
     """ Add 1 point to student's total points. 
     """
     if "teacher" or "mod" in [x.name.lower() for x in ctx.message.author.roles]:
       if student.id not in users:
-        # confirm that student is in users
-      # edit student.id's points in file	
+        print("")# confirm that student is in users
+        # edit student.id's points in file	
     else:
       await message.channel.send("You don't have permission to access this command!")
 
-  @commands.command()
-  async def remove(self, ctx, student: discord.Member):
-    """ Subtract 1 point from student's total points.
-    """
-    if "teacher" or "mod" in [x.name.lower() for x in ctx.message.author.roles]:
-      # edit student.id's points in file
-    else:
-      await message.channel.send("You don't have permission to access this command!")
+@bot.event
+async def remove(self, ctx, student: discord.Member):
+  """ Subtract 1 point from student's total points.
+  """
+  if "teacher" or "mod" in [x.name.lower() for x in ctx.message.author.roles]:
+    print("")# edit student.id's points in file
+  else:
+    await message.channel.send("You don't have permission to access this command!")
 
-  @commands.command()
-  async def mypoints(self, ctx):
-    id = message.author.id
-    if id in users:
-      await message.channel.send("<@" + str(id) + "has {0.pts}.".format(users[id]['points'])) # tentative lmao
-    else:
-      await message.channel.send("You don't have any points yet! Ask your teacher to set up your profile.")
+@bot.event
+async def mypoints(self, ctx):
+  id = message.author.id
+  if id in users:
+    await message.channel.send("<@" + str(id) + "has {0.pts}.".format(users[id]['points'])) # tentative lmao
+  else:
+    await message.channel.send("You don't have any points yet! Ask your teacher to set up your profile.")
     
 
 # Take attendance from Students
 @bot.event
-async def on_ready():
+async def attendance():
   channel = client.get_channel(xxx)
   text = "Attendance! React to let the teach know you are here!"
   moji = await bot.send_message(channel, text)
@@ -82,25 +88,29 @@ async def on_reaction_add(reaction, user):
     role = discord.utils.get(user.server.roles, name="Present")
     await client.add_roles(user, role)
 
+
+HALLWAY = bot.get_channel(838153995319115796)
+CLASSROOM = bot.get_channel(838130302512922676)
+TEACHER_TEXT = bot.get_channel(838184342999924776)
+
 # hall pass
-class HallPass(commands.Cog):
-  def __init__(self, bot):
-      self.bot = bot
+@bot.command()
+async def away(ctx):
+  """ Move the user into the hallway voice channel."""
+  student = ctx.author
+  
+  # if student.voice is None or student.voice.channel != CLASSROOM:
+  #   return await ctx.send("<@" + str(student.id) + ">, you aren't in the classroom right now!")
+  await student.move_to(HALLWAY)
+  await TEACHER_TEXT.send("<@" + str(student.id) + '> is away from their computer.')
 
-  @commands.command()
-  async def away(self, ctx):
-    """ Send message to alert that this user is away. This will also move the user into a muted voice channel.
-    """
-    student = message.author.id
-    voice_channel = discord.utils.find(lambda x: x.name.lower() == 'hall channel', message.server.channels)
-    await bot.move_to(student, voice_channel)
-    await 
-    
+@bot.command()
+async def back(ctx):
+  """ Move the user back into the classroom voice channel."""
+  student = ctx.author
+  await student.move_to(CLASSROOM)
+  await TEACHER_TEXT.send("<@" + str(student.id) + '> is back in the classroom.')
 
-  @commands.command()
-  async def back(self, ctx):
-    """Send message to alert that this user has returned. This will also move the user back into the classroom voice channel. 
-    """
 
-bot.run(os.envrion['BOT_TOKEN'])
+bot.run(os.environ['BOT_TOKEN'])
 
